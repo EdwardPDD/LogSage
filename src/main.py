9b7e75b5e570e2ff2d6fa6cd7226738ab4bde6c9
EdwardPDD <Edsrevolution@gmail.com>
2026-07-10 06:27:33 +0000

    Add interactive menu and user input

--- src/main.py
+++ src/main.py
@@ -3,9 +3,40 @@ def display_banner():
     print('        LogSage Security Tool      ')
     print('===================================')
 
+
+def display_menu():
+    print('1. Analyze Linux Logs')
+    print('2. Analyze Windows Logs')
+    print('3. Exit')
+
+
 def main():
-    display_banner()
-    
-    
+    while True:
+        display_banner()
+        display_menu()
+
+        choice = input('select an option: ')
+
+
+        if choice == '1':
+            print()
+            print('Analyzing Linux Logs...')
+
+        elif choice == '2':
+            print()
+            print('Analyzing Windows Logs...')
+
+        elif choice == '3':
+            print()
+            print('Exiting the program...')
+            break
+            
+
+        else:
+            print()
+            print('Invalid option. Please try again.')
+            
+
+
 if __name__ == '__main__':
     main()
