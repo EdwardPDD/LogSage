500ec0cc30e3a81500e0e268cb068558a9c9efef
EdwardPDD <Edsrevolution@gmail.com>
2026-07-10 05:46:45 +0000

    create application architecture and startup banner

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def display_banner(): 
+    print('===================================')
+    print('        LogSage Security Tool      ')
+    print('===================================')
+
+def main():
+    display_banner()
+    
+    
+if __name__ == '__main__':
+    main()
